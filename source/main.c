eb64a3cc9c06a15cc34bfe3aa970ff791e799ad5
Al-Azif <alazif@miskatonic.io>
2021-01-27 17:48:43 -0800

    Change "socket" to "debug"

--- source/main.c
+++ source/main.c
@@ -28,7 +28,7 @@ int _main(struct thread *td) {
   printf_notification("Enabled updates!");
 
 #ifdef DEBUG_SOCKET
-  printf_socket("\nClosing socket...\n\n");
+  printf_debug("Closing socket...\n");
   SckClose(DEBUG_SOCK);
 #endif
 
